92576ac005ed8536048c623163f1d29c1f34bf9e
oogy <tylerweldon94@gmail.com>
2021-02-14 18:48:06 -0500

    add variable descriptions

--- variables.tf
+++ variables.tf
@@ -1,19 +1,23 @@
 variable "flight_count" {
-  type    = number
-  default = 1
+  description = "Number of flight nodes to deploy."
+  type        = number
+  default     = 1
 }
 
 variable "machine_type" {
-  type    = string
-  default = "hfc-6c-24gb"
+  description = "Vultr server plan."
+  type        = string
+  default     = "hfc-6c-24gb"
 }
 
 variable "region" {
-  type    = string
-  default = "ewr"
+  description = "Vultr server location."
+  type        = string
+  default     = "ewr"
 }
 
 variable "astra_image_version" {
-  type    = string
-  default = ""
+  description = "Astra image to use."
+  type        = string
+  default     = "latest"
 }
